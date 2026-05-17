c064731a379d242d16d875cab181f7867168f07e
JulaJX <jacekeo007@gmail.com>
2026-05-17 15:00:06 +0200

    create structure od model classes

---
+++ Models/Component.cs
@@ -0,0 +1,15 @@
+namespace Cwiczenia7Api.Models;
+
+public class Component
+{
+    public string Code { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+
+    public int ComponentManufacturersId { get; set; }
+    public int ComponentTypesId { get; set; }
+
+    public ComponentManufacturer Manufacturer { get; set; } = null!;
+    public ComponentType Type { get; set; } = null!;
+    public ICollection<PcComponent> PcComponents { get; set; } = new List<PcComponent>();
+}
---
+++ Models/PcComponent.cs
@@ -0,0 +1,11 @@
+namespace Cwiczenia7Api.Models;
+
+public class PcComponent
+{
+    public int PCId { get; set; }
+    public string ComponentCode { get; set; } = null!;
+    public int Amount { get; set; }
+
+    public Pc Pc { get; set; } = null!;
+    public Component Component { get; set; } = null!;
+}
